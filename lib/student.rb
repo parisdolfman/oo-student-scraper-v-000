14a7ffa69371500b1487cfb4c1a91e1eafcaed75
Paris Dolfman <parisdolfman@gmail.com>
2019-11-11 19:46:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+    student_hash.each do |attribute, value|
+        self.send("#{attribute}=", value)
+  @@all << self 
   end
 
   def self.create_from_collection(students_array)
