623de42b3b9f6d211f9b7d52011d426587232358
Paris Dolfman <parisdolfman@gmail.com>
2019-11-11 19:47:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,8 +21,8 @@ end
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, value|
           self.send("#{attribute}=", value)
-      end 
-    self 
+      end
+    self
   end
 
   def self.all
